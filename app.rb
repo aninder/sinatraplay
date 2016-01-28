f60e39e4ae981ab749f6540039222ad680d59b0f
aninder <aninder@gmail.com>
2016-01-29 01:39:21 +0530

    configure sprockets

--- app.rb
+++ app.rb
@@ -1,37 +1,52 @@
 require 'sinatra'
 require 'shotgun'
 require 'haml'
+require 'sprockets'
 
 configure do
   set :haml, {:format => :html5}
+  set :assets, Sprockets::Environment.new
+
   enable :sessions
   set :sessions,
-      :key          => 'SESSION_ID',
-      :httponly     => true,
-      :secure       => false,
+      :key => 'SESSION_ID',
+      :httponly => true,
+      :secure => false,
       :expire_after => 60*60*24, # == one day
-      :secret       => ENV['SESSION_SECRET'] || "changeme"
+      :secret => ENV['SESSION_SECRET'] || "changeme"
   use Rack::MethodOverride
-# require 'rack-flash'
-# use Rack::Flash
 
-# register Sinatra::ConfigFile
-# config_file 'config.yml'
-# set :public_folder, "#{File.dirname(__FILE__)}/public"
-# set :views, File.dirname(__FILE__) + '/views'
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+  # require 'rack-flash'
+  # use Rack::Flash
 
-# before '/admin/*' do
-# content_type :txt
-# check logged in
-# end
+  # register Sinatra::ConfigFile
+  # config_file 'config.yml'
+  # set :public_folder, "#{File.dirname(__FILE__)}/public"
+  # set :views, File.dirname(__FILE__) + '/views'
 
+  # before '/admin/*' do
+  # content_type :txt
+  # check logged in
 end
 
 before do
-  content_type 'application/json'
+  content_type 'html'
+end
+helpers do
 end
 
-helpers do
+# Configure sprockets
+settings.assets.append_path "js"
+settings.assets.append_path "css"
+get "/js/:file.js" do
+  content_type "application/javascript"
+  settings.assets["#{params[:file]}.js"]
+end
+get "/css/:file.css" do
+  content_type "text/css"
+  settings.assets["#{params[:file]}.css"]
 end
 
 get "/" do
@@ -42,10 +57,11 @@ __END__
 
 @@ layout
 %html
+ %head
+  %script(type="text/javascript" src="js/application.js")
+  %link{:href => "css/application.css", :rel => "stylesheet", :type => "text/css"}
  %body
   =yield
 
 @@ home
-%p Hello Sinatra
-
-
+%p.well Hello Sinatra
